bccddf9d1092d80dc076f5d153c37fce019d192b
Jason Tatton <jtatton@drwholdings.com>
2026-01-13 12:18:23 +0000

    finsihed multiply by 10, 100, 1000 questoins

--- src/games/calculations/x10/questions.tsx
+++ src/games/calculations/x10/questions.tsx
@@ -1,8 +1,9 @@
 import {Difficulty, Question} from "../../../common/QuestionCardSeries";
 import {
-    MULTI_CHOICE_DIST,
+    niceRound,
     randomBoolean,
-    randomIntUpTo,
+    roundUpToNearest10,
+    sampleIntRange,
     shuffle,
     weightedSample,
     WeightingProfile
@@ -10,6 +11,30 @@ import {
 
 
 type Unit = 10 | 100 | 1000;
+
+type NumRange = [number, number];
+
+const small: NumRange = [1, 100];
+const big: NumRange = [100, 10000];
+const decimal: NumRange = [0, 1];
+
+const difficultyToRange: Record<Difficulty, WeightingProfile<NumRange>> = {
+    Easy: [
+        {value: small, weight: 50},
+        {value: big, weight: 25},
+        {value: decimal, weight: 25}
+    ],
+    Medium: [
+        {value: small, weight: 20},
+        {value: big, weight: 60},
+        {value: decimal, weight: 20}
+    ],
+    Hard: [
+        {value: small, weight: 0},
+        {value: big, weight: 20},
+        {value: decimal, weight: 80}
+    ],
+};
 
 const difficultyToMultiplier: Record<Difficulty, WeightingProfile<Unit>> = {
     Easy: [
@@ -29,162 +54,108 @@ const difficultyToMultiplier: Record<Difficulty, WeightingProfile<Unit>> = {
     ],
 };
 
-const unitToBlurb: Record<Unit, string> = {
-    10: 'ten',
-    100: 'hundred',
-    1000: 'thousand',
-};
-
-
-const msbOffsetDist: WeightingProfile<number> = [
-    {value: -1, weight: 20},
-    {value: 0, weight: 30},
-    {value: 1, weight: 30},
-    {value: 2, weight: 20}
-];
-
-type RoundingScheme = 'Nearest' | 'Up' | 'Down';
-
-const roundingSchemeBlurb: Record<RoundingScheme, string> = {
-    Up: 'up to the nearest',
-    Down: 'down to the nearest',
-    Nearest: 'to the nearest',
-}
-
-const schemeDist: WeightingProfile<RoundingScheme> = [
-    {value: 'Nearest', weight: 34},
-    {value: 'Up', weight: 33},
-    {value: 'Down', weight: 33},
-];
-
-
-function roundToUnit(value: number, unit: Unit, mode: RoundingScheme): number {
-    if (unit <= 0) throw new Error("Unit must be a positive number");
-
-    const divided = value / unit;
-
-    switch (mode) {
-        case "Up":
-            return Math.ceil(divided) * unit;
-        case "Down":
-            return Math.floor(divided) * unit;
-        case "Nearest":
-            return Math.round(divided) * unit;
-    }
-}
-
-const unitDigitDist: WeightingProfile<number> = [
-    {value: 1, weight: 9},
-    {value: 2, weight: 9},
-    {value: 3, weight: 9},
-    {value: 4, weight: 9},
-    {value: 5, weight: 28},//28% of the time we have 5 as this is the most interesting number for rounding
-    {value: 6, weight: 9},
-    {value: 7, weight: 9},
-    {value: 8, weight: 9},
-    {value: 9, weight: 9},
-];
-
-function randomWithOverride(totalDigs: number, overrideWhat: number, overrideDigit: number): number {
-    if (totalDigs < 1) {
-        throw new Error(`totalDigs (${totalDigs}) must be >= 1`);
-    }
-    if (overrideWhat < 1 || overrideWhat > totalDigs) {
-        throw new Error(`overrideWhat (${overrideWhat}) must be between 1 and ${totalDigs}`);
-    }
-    if (overrideDigit < 0 || overrideDigit > 9) {
-        throw new Error('overrideDigit must be between 0 and 9');
-    }
-
-    const digits: number[] = Array.from({length: totalDigs}, () =>
-        Math.floor(Math.random() * 10)
-    );
-
-    // Prevent leading zero if n > 1
-    if (totalDigs > 1 && digits[0] === 0) {
-        digits[0] = Math.floor(Math.random() * 9) + 1;
-    }
-
-    // Override m-th (1‑based index)
-    digits[overrideWhat - 1] = overrideDigit;
-
-    return parseInt(digits.join(''), 10);
-}
 
 const max_attempts = 100; // jsut in case
 
-function generateBogusAnswers(
-    originalNumber: number,
-    answer: number,
-    unit: Unit
-): number[] {
-    function genWithRetry(func: () => number) {
-        let attempt = func()
-        let cnt = 0;
-        // eslint-disable-next-line eqeqeq
-        while (attempt <= 0 || attempt == answer || attempt > 3999) {
-            attempt = func()
-            if (cnt++ > max_attempts) {
-                break;
-            }
+function genWithRetry(formatIfDecimal: (_: number) => string, answer: number, func: () => number) {
+    let attempt = func()
+    let cnt = 0;
+    // eslint-disable-next-line eqeqeq
+    while (attempt <= 0 || attempt == answer || attempt > 3999) {
+        attempt = func()
+        if (cnt++ > max_attempts) {
+            break;
         }
-        return attempt;
     }
-
-    const offByOne = genWithRetry(() => answer + (unit * (randomBoolean() ? -1 : 1)));
-    const offByBitMore = genWithRetry(() => answer + (unit * randomIntUpTo(5) * (randomBoolean() ? -1 : 1)));
-    const offByByLot = genWithRetry(() => answer + (unit * randomIntUpTo(15) * (randomBoolean() ? -1 : 1)));
-
-    return [offByOne, offByBitMore, offByByLot];
+    return formatIfDecimal(attempt);
 }
 
-type Operation = 'multiply' | 'divide';
+function generateBogusAnswers(
+    formatIfDecimal: (_: number) => string,
+    multiply: boolean,
+    answer: number,
+    unit: Unit
+): string[] {
+    if (multiply) {
+        const offByOne = genWithRetry(formatIfDecimal, answer, () => (answer / unit) * (unit * 10));
+        const offByBitMore = genWithRetry(formatIfDecimal, answer, () => (answer / unit) * (unit * 100));
+        const offByByLot = genWithRetry(formatIfDecimal, answer, () => (answer / unit) * (unit * 1000));
+
+        return [offByOne, offByBitMore, offByByLot];
+    } else {
+        const offByOne = genWithRetry(formatIfDecimal, answer, () => (answer * unit) / (unit * 10));
+        const offByBitMore = genWithRetry(formatIfDecimal, answer, () => (answer * unit) / (unit * 100));
+        const offByByLot = genWithRetry(formatIfDecimal, answer, () => (answer * unit) / (unit * 1000));
+
+        return [offByOne, offByBitMore, offByByLot];
+    }
+}
+
+function isLaDecimal(num: number): boolean {
+    return num % 1 !== 0;
+}
 
 
 export function makeQuestionBank(toMake: number, difficulty: Difficulty): Question[] {
 
     return Array.from({length: toMake}, (_) => {
-        const unit = weightedSample(difficultyToMultiplier[difficulty]);
+        const multiplier = weightedSample(difficultyToMultiplier[difficulty]);
 
+        const range = weightedSample(difficultyToRange[difficulty]);
+        //75% bias towards end with zero
 
-        const unitDigits = Math.log(unit) / Math.log(10) + 1;
-        const digits = unitDigits + weightedSample(msbOffsetDist)
+        let sourceNumber: number;
 
-        let num = randomWithOverride(digits, digits - unitDigits + 2, weightedSample(unitDigitDist));
-        if (num < unit) {
-            // avoid "what is 7 to the nearest 100"
-            num += unit
+        const isDecimal = range === decimal;
+
+        if (isDecimal) {
+            // work backwards
+            const ans = Math.random();
+            sourceNumber = niceRound(ans / multiplier);
+        } else {
+            sourceNumber = sampleIntRange(range[0], range[1]);
+
+            if (randomBoolean(75)) {
+                sourceNumber = niceRound(roundUpToNearest10(sourceNumber));
+            }
         }
 
-        const scheme = weightedSample(schemeDist);
-        const answer = roundToUnit(num, unit, scheme);
-        const unitToNearestStr = randomBoolean() ? `${unit}` : unitToBlurb[unit];
+        let answer = sourceNumber * multiplier;
 
-        const multichoice = weightedSample(MULTI_CHOICE_DIST[difficulty]);
 
-        const operation: Operation = randomBoolean() ? 'multiply' : 'divide';
-        switch (operation) {
-            case 'multiply':
-                return {
-                    question: `multiply ${num} by ${roundingSchemeBlurb[scheme]} `,
-                    explain: [`${num} x ${roundingSchemeBlurb[scheme]} is ${answer.toLocaleString('en-US')}`],
-                    answer: answer,
-                    answers: multichoice ? shuffle([...generateBogusAnswers(num, answer, unit), answer]) : [],
-                    questionDifficulty: difficulty,
-                    answerFormat: multichoice ? undefined : 'DecimalInput',
-                };
-            case 'divide':
-                return {
-                    question: `round ${num} ${roundingSchemeBlurb[scheme]} ${unitToNearestStr}`,
-                    explain: [`${num} rounded ${roundingSchemeBlurb[scheme]} ${unitToNearestStr} is ${answer.toLocaleString('en-US')}`],
-                    answer: answer,
-                    answers: multichoice ? shuffle([...generateBogusAnswers(num, answer, unit), answer]) : [],
-                    questionDifficulty: difficulty,
-                    answerFormat: multichoice ? undefined : 'DecimalInput',
-                };
+        if (randomBoolean()) {
+
+            const formatIfDecimal = (num: number): string => {
+                return isLaDecimal(num) ? num.toFixed(4) : num.toLocaleString('en-US');
+            }
+
+            return {
+                question: randomBoolean(75) ? `Multiply ${formatIfDecimal(sourceNumber)} by ${formatIfDecimal(multiplier)} ` : `${formatIfDecimal(sourceNumber)} x ${formatIfDecimal(multiplier)} `,
+                explain: [`${formatIfDecimal(sourceNumber)} x ${multiplier} is ${formatIfDecimal(answer)}`],
+                answer: formatIfDecimal(answer),
+                answers: shuffle([...generateBogusAnswers(formatIfDecimal, true, answer, multiplier), formatIfDecimal(answer)]),
+                questionDifficulty: difficulty,
+                answerFormat: undefined,
+            };
+        } else {
+            const temp = answer;
+            answer = sourceNumber;
+            sourceNumber = temp;
+
+            const dps = Math.max(4, Math.log10(answer)) + Math.log10(multiplier);
+            const formatIfDecimal = (num: number): string => {
+                return isLaDecimal(num) ? num.toFixed(dps) : num.toLocaleString('en-US');
+            }
+
+            return {
+                question: randomBoolean(75) ? `Divide ${formatIfDecimal(sourceNumber)} by ${formatIfDecimal(multiplier)} ` : `${formatIfDecimal(sourceNumber)} / ${formatIfDecimal(multiplier)} `,
+                explain: [`${formatIfDecimal(sourceNumber)} / ${multiplier} is ${formatIfDecimal(answer)}`],
+                answer: formatIfDecimal(answer),
+                answers: shuffle([...generateBogusAnswers(formatIfDecimal, false, answer, multiplier), formatIfDecimal(answer)]),
+                questionDifficulty: difficulty,
+                answerFormat: undefined,
+            };
         }
-
-
     });
 
 }
